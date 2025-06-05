3313e35c528ce2f36a7de4569f7b07ae0ee1e05b
Victor Nogueira <victor.nogueira@tc.com.br>
2025-06-05 14:03:56 -0300

    tentando resolver

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
-import * as vscode from 'vscode';
-import { DeepSeekChatProvider } from './DeepSeekChatProvider';
+import * as vscode from "vscode";
+import { DeepSeekChatProvider } from "./DeepSeekChatProvider";
 
 export function activate(context: vscode.ExtensionContext) {
   const provider = new DeepSeekChatProvider();
 
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider('chatView', provider)
+    vscode.window.registerWebviewViewProvider("deepseekChat", provider)
   );
 }
 
